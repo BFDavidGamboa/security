170ef75c9ecf3c61bed1a3abd6b17787d38b2f4c
BFDavidGamboa <david.gamboa@blankfactor.com>
2023-09-01 15:40:34 -0500

    CreatedRole and UserRepository to manage the users and its roles through the application, Also created JWTService and JwtAuthenticationFilter to create a jwt with a generated key making use of the input parameters.

---
+++ src/main/java/com/david/security/testing/BananaChecks.java
@@ -0,0 +1,5 @@
+package com.david.security.testing;
+
+public class BananaChecks {
+    public static boolean containsBananas(String[] )
+}
--- src/main/java/com/david/security/user/User.java
+++ src/main/java/com/david/security/user/User.java
@@ -1,10 +1,16 @@
-package com.david.security.user;
+ package com.david.security.user;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 //This will create it's proper annotations
 @Data
@@ -17,7 +23,7 @@ import lombok.NoArgsConstructor;
 //Is intended for use in the database
 @Table(name = "_user")
 //Will provide the database for this user
-public class User {
+public class User implements UserDetails {
     //This value table will crete a table based in the values provided by this annotation
     //There are other options as well
     //@GeneratedValue(strategy = GenerationType.TABLE)
@@ -30,4 +36,41 @@ public class User {
     private String email;
     private String password;
 
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+     }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
